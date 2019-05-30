a561efb2cee0c9448fa009bf7cfe77d2b209f602
aminpaydar <mapsgp920@elenoon.ir>
2019-05-30 19:08:38 +0430

    ty

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 if use_dropout:
-    model.add(Dropout(0.75))
+    model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding='same'))
 model.add(Activation('relu'))
@@ -47,7 +47,7 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 if use_dropout:
-    model.add(Dropout(0.75))
+    model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(512))
